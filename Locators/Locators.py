a8c76b8cbea2e7538b3f48269a795bfda60439c2
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-12-27 01:03:25 +0600

    Update Locators.py

--- Locators/Locators.py
+++ Locators/Locators.py
@@ -7,10 +7,25 @@ driver.implicitly_wait(5000)
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
 
-#Searching using ID, Name & Class Locator
+# Searching elements using ID, Name & Class Locator
 driver.find_element(By.ID, "small-searchterms").clear()
 driver.find_element(By.NAME, "q").send_keys("Apple")
 driver.find_element(By.CLASS_NAME, "search-box-button").click()
 print("Button Pressed")
+
+# Going Back to Homepage
 driver.back()
+
+# Finding Element using link text
+driver.find_element(By.LINK_TEXT, 'Build your own computer').click()
+# Going Back to Homepage
+driver.back()
+
+
+# Finding element using partial link text
+driver.find_element(By.PARTIAL_LINK_TEXT, 'HTC').click()
+driver.back()
+
+
+driver.quit()
 
