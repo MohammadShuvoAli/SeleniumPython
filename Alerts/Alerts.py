6f3d2f64d0af1ba7944d04d8b3cc71dfcb84cbfe
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-12-30 22:41:02 +0600

    Update Alerts.py

--- Alerts/Alerts.py
+++ Alerts/Alerts.py
@@ -1,10 +1,23 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(5)
 
 driver.get("https://the-internet.herokuapp.com/javascript_alerts")
 
+# opens alert window
+driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']").click()
+
+alert_window = driver.switch_to.alert
+
+print(alert_window.text)
+
+alert_window.send_keys("Shuvo")
+# alert_window.accept() # ok
+alert_window.dismiss() # cancel
+
+time.sleep(10)
 driver.quit()
