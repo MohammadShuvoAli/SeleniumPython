73e4c70d2ea7b81784ec0b436f161a0b21ce5176
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-12-30 19:38:14 +0600

    Update Broken_Links.py

--- Links/Broken_Links.py
+++ Links/Broken_Links.py
@@ -1,5 +1,4 @@
-import time
-
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -8,5 +7,23 @@ driver.maximize_window()
 
 driver.get("http://www.deadlinkcity.com/")
 
-time.sleep(3)
+all_links = driver.find_elements(By.TAG_NAME, 'a')
+
+count = 0
+#
+for link in all_links:
+    url = link.get_attribute('href')
+    try:
+        res = requests.head(url)
+    except:
+        None
+
+    if res.status_code>=400:
+        print(url," is broken link")
+        count += 1
+    else:
+        print(url, " is valid link")
+
+print("Total number of broken links: ", count)
+
 driver.quit()
