b38b3b309173c19db281d2a5b3bfd52c109fef70
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-02 00:47:40 +0600

    Update mouse_right_click.py

--- Mouse_Operations/mouse_right_click.py
+++ Mouse_Operations/mouse_right_click.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
@@ -8,7 +9,10 @@ driver.implicitly_wait(10)
 
 driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
 
+button = driver.find_element(By.XPATH, '/html/body/div/section/div/div/div/p/span')
 
+act = ActionChains(driver)
+act.context_click(button).perform()
 
 time.sleep(2)
 driver.quit()
