16a0717b9d3ebab1044a97d055cac7efe81a52bb
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-02 00:41:33 +0600

    Create mouse_right_click.py

---
+++ Mouse_Operations/mouse_right_click.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.get("https://demo.nopcommerce.com/")
+
+
+
+time.sleep(2)
+driver.quit()
