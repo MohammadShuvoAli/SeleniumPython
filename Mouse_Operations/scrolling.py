adb267886d4e6ecb9b1be873c5ff8a9adda6170a
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-02 02:42:51 +0600

    Update scrolling.py

---
+++ Mouse_Operations/scrolling.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.get("https://flagpedia.net/index")
+
+
+
+time.sleep(2)
+driver.quit()
