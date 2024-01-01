486ed6d1aa47c0bb5a1fed57228773cbc4feaecc
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-02 01:05:26 +0600

    Update drag_and_drop.py

--- Mouse_Operations/drag_and_drop.py
+++ Mouse_Operations/drag_and_drop.py
@@ -9,5 +9,11 @@ driver.implicitly_wait(10)
 
 driver.get("https://testautomationpractice.blogspot.com/")
 
+target_element = driver.find_element(By.XPATH, "//p[normalize-space()='Drag me to my target']")
+source_element = driver.find_element(By.XPATH, "//div[@id='droppable']")
+
+act = ActionChains(driver)
+act.drag_and_drop(target_element, source_element).perform()
+
 time.sleep(2)
 driver.quit()
