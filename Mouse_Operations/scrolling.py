8cfc2308efbcfbc212def8a97d98e47306de6f06
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-02 02:57:35 +0600

    Update scrolling.py

--- Mouse_Operations/scrolling.py
+++ Mouse_Operations/scrolling.py
@@ -1,15 +1,32 @@
 import time
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.maximize_window()
 driver.implicitly_wait(10)
 
 driver.get("https://flagpedia.net/index")
 
+# 1. scroll down by pixel
+driver.execute_script("window.scrollBy(0, 3000)", "")
+value = driver.execute_script("return window.pageYOffset;")
+print("Number of pixel moved:", value)
 
+# 2. scroll down till the element is visible
+flag = driver.find_element(By.XPATH,"//img[@alt='Flag of Bangladesh']")
+driver.execute_script("arguments[0].scrollIntoView()", flag)
+value = driver.execute_script("return window.pageYOffset;")
+print("Number of pixel moved:", value)
+
+# 3. scroll down till the end
+driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+value = driver.execute_script("return window.pageYOffset;")
+print("Number of pixel moved:", value)
+
+# 4. scroll up to starting position
+driver.execute_script("window.scrollBy(0,-document.body.scrollHeight)")
+value = driver.execute_script("return window.pageYOffset;")
+print("Number of pixel moved:", value)
 
 time.sleep(2)
 driver.quit()
